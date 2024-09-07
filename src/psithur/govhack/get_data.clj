eb4be8e307e1c83c0af61b1629fdda5c8c6544c2
xlfe <felixb@gmail.com>
2024-09-07 17:51:27 +1000

    some how missed 2016

--- src/psithur/govhack/get_data.clj
+++ src/psithur/govhack/get_data.clj
@@ -337,10 +337,66 @@
    "chamber/hansardr/50ef4858-02bd-437b-a64f-599769ecfec6/0000"
    "chamber/hansardr/9b96ae59-96ca-4e39-b984-8b520b432ef5/0000"])
 
+(def more
+  [
+   "chamber/hansardr/246a269b-5745-4465-8d60-10707e9a72f2/0000"
+   "chamber/hansardr/d39a0737-7c7a-4f79-8bb5-2c3ae841d1cb/0000"
+   "chamber/hansardr/aaec4b82-2411-445e-9df4-a787c30c60c2/0000"
+   "chamber/hansardr/a8ddabc5-4310-4bda-a2fd-c9e6a54e12dc/0000"
+   "chamber/hansardr/66ebdf1a-c676-47e2-b941-deb23e551e94/0000"
+   "chamber/hansardr/7ef9bd10-ec92-4de4-9372-a92d6a12d7ef/0000"
+   "chamber/hansardr/11a9f69e-a789-4d75-854d-edb89ae01a41/0000"
+   "chamber/hansardr/02525cdf-c0a5-4a3a-a1f4-9e9219e30bf6/0000"
+   "chamber/hansardr/cbd6a0a1-e9e6-444c-8403-340b3c2ab63d/0000"
+   "chamber/hansardr/dff2f362-577c-44bb-a29c-3d98a57a635a/0000"
+   "chamber/hansardr/115c3603-d1aa-4e7e-8ec1-5ec3e40edc8a/0000"
+   "chamber/hansardr/3e7fc91c-2a08-4853-95e3-77e2c5b615c5/0000"
+   "chamber/hansardr/5579af19-5789-4c92-af4a-cf10f27c8e5a/0000"
+   "chamber/hansardr/563b33e2-7088-4c58-9ed7-833adbfece13/0000"
+   "chamber/hansardr/0c15c959-ca6c-4549-90fc-1f1900a51b06/0000"
+   "chamber/hansardr/b9fdd3c0-0b71-4ff7-9a54-deaf6cfc2db3/0000"
+   "chamber/hansardr/2886ba5c-1252-48d9-9edb-ab1ff653e53a/0000"
+   "chamber/hansardr/691348af-18d4-4d85-9c31-ae0b99d4dc6c/0000"
+   "chamber/hansardr/bca2d1b1-5064-4355-8d61-40f3959f43da/0000"
+   "chamber/hansardr/72a020b3-432a-4737-af9b-1927e6fcaa6e/0000"
+   "chamber/hansardr/d70ff86d-1625-4f80-91fb-dc45bba5e32e/0000"
+   "chamber/hansardr/2f71294b-3aa4-45d8-ae01-c94dbd36aa02/0000"
+   "chamber/hansardr/b610d191-4da6-422a-8c90-055755970d9e/0000"
+   "chamber/hansardr/5ad31cc6-5b90-4856-adc8-83b8e5eb87ac/0000"
+   "chamber/hansardr/1133bdef-2731-42fb-a226-6522e1a8fec5/0000"
+   "chamber/hansardr/f706773d-28cd-4fb7-b62a-9ed884a645d8/0000"
+   "chamber/hansardr/5ad84e4d-3946-42fa-bf30-ffdec783bc3d/0000"
+   "chamber/hansardr/d961ab05-d592-46b4-8a71-e41c76b2d8ae/0000"
+   "chamber/hansardr/b9050997-2c6e-4bd2-91d4-ced0b8104a75/0000"
+   "chamber/hansardr/bf6692fe-063a-41a2-9ebb-5de513f31940/0000"
+   "chamber/hansardr/9de5f081-9ccf-4f72-a05a-02462c1f3a0c/0000"
+   "chamber/hansardr/53c9fd8e-e207-4e59-aa4a-a1953f715e20/0000"
+   "chamber/hansardr/5231190c-6a41-40bb-a062-0d655dea71f6/0000"
+   "chamber/hansardr/3bef5bfd-10a1-42ac-8cdf-41e8b9c0f758/0000"
+   "chamber/hansardr/f2541705-5a09-4607-bf3f-1d6fd9b611ff/0000"
+   "chamber/hansardr/f86cb155-06ef-4ce4-a81e-4ab253e0a30f/0000"
+   "chamber/hansardr/dc58b6e4-9f62-4cac-8b47-8eedf2e5c75d/0000"
+   "chamber/hansardr/bb53e009-91c0-438a-af01-a60fc481fdbf/0000"
+   "chamber/hansardr/49c9d51c-41fd-4c99-9918-985afdc5fb8e/0000"
+   "chamber/hansardr/a2ba36f1-ee24-4aec-9f8e-b647c7fc92ce/0000"
+   "chamber/hansardr/35853536-eb47-48bb-9a4e-8044d466345a/0000"
+   "chamber/hansardr/e089c8c3-75b7-4858-80ac-58b4e4c6e749/0000"
+   "chamber/hansardr/6af91160-b914-4fa6-8c73-1b8537a2cfb7/0000"
+   "chamber/hansardr/715fab49-01b7-4d20-89b0-5496732f6bc2/0000"
+   "chamber/hansardr/ad2aa11a-4ece-4039-a88a-4620aa6c9831/0000"
+   "chamber/hansardr/9b169b3b-768b-49e5-aabb-ed05f3ce0ebb/0000"
+   "chamber/hansardr/d8b39000-045b-46aa-b142-3ba818466f26/0000"
+   "chamber/hansardr/db7bb414-c772-4f68-9fbb-9c91c3913de7/0000"
+   "chamber/hansardr/f30e7743-6615-4360-8f82-11ec68352a9b/0000"
+   "chamber/hansardr/0f8db77b-d8d3-4de1-958b-18847b30d0e2/0000"
+   "chamber/hansardr/b564519e-8c2b-4dbf-a1e8-8c9334a04537/0000"])
+
+
+
 (defn get-other-hansards!
   "these weren't picked up originally because the page uses javascript to hide older transcripts"
   []
-  (let [hansards hidden]
+  (let [hansards more]
     (doseq [hansard hansards]
       (let [fp (file-path-exists? hansard)]
         (if (true? fp) (prn "Already have" hansard) (spit fp (get-hansard hansard)))))))
